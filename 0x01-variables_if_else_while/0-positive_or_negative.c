672e3b590626712492f17ed7b515188d4fb03548
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-14 05:36:14 -0700

    if else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * msin - Entry point
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
