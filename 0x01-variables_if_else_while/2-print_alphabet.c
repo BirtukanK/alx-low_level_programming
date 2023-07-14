ec89ca077d1d9cebf36d1a81fde7d85308464d61
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-14 11:50:20 -0700

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+}
+return (0);
+}
