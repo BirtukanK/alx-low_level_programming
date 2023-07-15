72c05848bd7ca571007557d4d125672f42126ea2
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-14 22:34:40 -0700

    print except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Succss)
+ */
+int main(void)
+{
+char lower = 'a';
+while (lower <= 'z')
+{
+if (lower != 'e' || lower != 'q')
+{
+putchar(lower);
+lower++;
+}
+}
+putchar('\n');
+return (0);
+}
