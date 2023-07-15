a6f637f803e8e27d391f44eaec4c972ead01dab3
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-15 07:16:26 -0700

    print numbers with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * This program will print numbers using putchar function
- * Return - Always 0 (Success)
+ * Description: This program will print numbers using putchar function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
