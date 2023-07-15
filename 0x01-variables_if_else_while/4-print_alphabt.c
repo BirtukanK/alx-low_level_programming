f8f8bb177ea12a4c6db0ef528e3f8b4acef890e5
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-14 22:38:04 -0700

    print except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@ int main(void)
 char lower = 'a';
 while (lower <= 'z')
 {
-if (lower != 'e' && lower != 'q')
+if (lower != 'e' || lower != 'q')
 {
 putchar(lower);
-lower++;
 }
+lower++;
 }
 putchar('\n');
 return (0);
