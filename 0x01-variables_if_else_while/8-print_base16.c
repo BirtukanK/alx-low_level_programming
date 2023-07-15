0fc51bdce823150fd1a2faaa9d92d59d2837852f
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-15 07:45:30 -0700

    print numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: this program that prints numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(48 + i);
+i++;
+}
+char ch = 'a';
+while (ch <= 'f')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
