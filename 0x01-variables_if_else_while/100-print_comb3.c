ed052bd5c33d4313428f4df094a9ef5b529119f2
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-16 04:01:50 -0700

    Print combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all combination of numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = 1; j <= 9; j++)
+{
+putchar(48 + i);
+putchar(48 + j);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
