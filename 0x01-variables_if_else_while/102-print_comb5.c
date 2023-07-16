49bd8f23057c12e8fbf7bbc404564b5c3cc1932f
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-16 09:56:05 -0700

    print all two digit combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 putchar('0' + i);
-putchar('0' + j);
+putchar('0' + i);
 putchar(' ');
-putchar('0' + j);
+putchar('0' + i);
 putchar('0' + j + 1);
 putchar(',');
 }
