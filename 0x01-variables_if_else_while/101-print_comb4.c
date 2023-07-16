f3273140ece09ec3eb7c699de3cf5a59ec3698ff
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-16 04:55:24 -0700

    print three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:  program that prints different combinations of three digits.
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 0; i <= 9; i++)
+{
+for (j = 1; j <= 9; j++)
+{
+for (k = 2; k <= 9; k++)
+{
+if (i != j && i != k && i < j && j < k)
+{
+putchar(48 + i);
+putchar(48 + j);
+putchar(48 + k);
+}
+if (i + j + k != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
