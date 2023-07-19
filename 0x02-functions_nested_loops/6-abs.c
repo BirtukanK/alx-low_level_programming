fb76d3fddc052e7b92d6a62699ec65f0ad0e194a
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-19 04:08:13 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ * @n : find absolute value of this integer
+ * Return: none
+ */
+int _abs(int)
+{
+_putchar(abs(int n));
+_putchar('\n');
+return (0);
+}
