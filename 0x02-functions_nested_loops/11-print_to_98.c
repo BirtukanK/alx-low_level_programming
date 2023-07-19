b943c73c8ef6ac0f4ffaf21641095c0e7637d2d8
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-19 13:21:11 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,18 @@ void print_to_98(int n)
 int i;
 if (n < 98)
 {
-for (i = n; i <= 98; i++)
+for (i = n; i < 98; i++)
 {
 printf("%i, ", i);
 }
 }
 else
 {
-for (i = n; i >= 98; i--)
+for (i = n; i > 98; i--)
 {
 printf("%i, ", i);
 }
 }
+printf("98");
 printf("\n");
 }
