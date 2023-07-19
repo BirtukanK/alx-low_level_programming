8f3a1c067e3e14c1dd19053ae3409aa2f592e5d0
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-19 01:53:07 -0700

    check if lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,18 @@
 #include "main.h"
+#include <ctype.h>
 /**
  * _islower - Entry point
+ * @c : character to check
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-int c;
 if (islower(c))
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
