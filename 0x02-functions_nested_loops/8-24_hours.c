ed954180d25a5423e56e56d4a897a0d971766654
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-19 06:40:21 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return:void
+ */
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar(i + '0'':'j + '0');
+_putchar('\n');
+}
+}
+return (0);
+}
