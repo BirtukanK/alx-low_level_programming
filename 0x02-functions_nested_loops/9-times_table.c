0d2e6348377fa6b1bb235e254498625603f3580f
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-19 11:18:30 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,10 +11,20 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j < 10; j++)
 {
 result = i * j;
+if (result <= 9)
+{
+_putchar(result);
+}
+else
+{
 _putchar((result / 10) + '0');
 _putchar((result % 10) + '0');
+}
+if (i + j != 9)
+{
 _putchar(',');
 _putchar(' ');
+}
 }
 _putchar('\n');
 }
