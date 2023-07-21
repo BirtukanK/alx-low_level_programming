e5323145bdd8c964d742e22ab2b8372c4fdf54ad
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-21 04:16:07 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+/**
+ * _isupper - a function which checks uppercase
+ * @c: character to check if it is upper or not
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
