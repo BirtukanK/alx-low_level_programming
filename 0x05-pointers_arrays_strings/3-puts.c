5b4af6fda4b01390d3920919d6f3c23df5d3204d
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-24 07:02:54 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string followed by a new line
+ * @str: character to print
+ * Return: none
+ */
+void _puts(char *str)
+{
+puts(str);
+}
