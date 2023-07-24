095833c0da824e88e79628f4839b1fdc46bba9c4
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-24 06:38:02 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns length of a string
+ * @s: character to check
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
