87f8d3ef51f0b232e97b3daa58611395d8f04e19
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-25 00:28:37 -0700

    print half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,19 @@
  */
 void puts_half(char *str)
 {
-int i, len;
+int i, len, n;
 len = strlen(str);
 if (len % 2 == 0)
 {
-for (i = len/2; i < len; i++)
+for (i = len / 2; i < len; i++)
 {
 _putchar(str[i]);
 }
 }
 else
 {
-len = (len - 1)/2;
-for (i = len / 2; i < len; i++)
+n = (len - 1) / 2;
+for (i = n; i < len; i++)
 {
 _putchar(str[i]);
 }
