62b1c0e0c29aa8481923f07a3de3d61e425d7a29
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-25 04:44:33 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - a function that copies a string
+ * @dest: string destnation
+ * @src: string source
+ * Return: character value
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
