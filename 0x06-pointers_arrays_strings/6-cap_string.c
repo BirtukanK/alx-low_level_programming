075acfdc3fb677a011bc0592ba5b1e450a75fac9
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-27 05:17:06 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,43 @@
 #include "main.h"
+
 /**
- * cap_string - a function which capitalize string
- * @c: string to change
- * Return: character
- */
-char *cap_string(char *c)
+ * *cap_string - capitalize words
+ * @str: pointer
+ * Return: capitalzied string
+*/
+
+char *cap_string(char *str)
 {
-int i, j, flag;
-char sep[] = ",\t;\n;.!?\"(){}";
-for (i = 0; c[i] != '\0'; i++)
-{
-flag = 0;
-if (i == 0)
-{
-flag = 1;
-}
-else
-{
-for (j = 0; sep[j] != '\0'; j++)
-{
-if (c[i - 1] == sep[j])
-{
-flag = 1;
-break;
-}
-}
-}
-if (flag == 1)
-{
-if (c[i] <= 'z' && c[i] >= 'a')
-{
-c[i] = c[i] - ('a' - 'A');
-}
-}
-}
-return (c);
+	char sep[] = ",\t;\n; .!?\"(){}";
+	int flag, i, ii;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		flag = 0;
+
+		if (i == 0)
+		{
+			flag = 1;
+		}
+		else
+		{
+			for (ii = 0; sep[ii] != '\0'; ii++)
+			{
+				if (str[i - 1] == sep[ii])
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+
+		if (flag == 1)
+		{
+			if (str[i] <= 'z' && str[i] >= 'a')
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
+	}
+	return (str);
 }
