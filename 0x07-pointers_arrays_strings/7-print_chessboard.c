42522e775ed370a72e562afcb4d69b8466339f89
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-31 02:02:29 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - a function that prints chessboard
+ * @a: array to display
+ * Return: none
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i <= 8; i++)
+{
+for (j = 0; j <= 7; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
+
