e0a4bd51870de39805c00e09285e75d4fee8c5af
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-31 04:22:16 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: the string from which we locate a characte
+ * @c: character to locate
+ * Return: 0 or 1
+ */
+char *_strchr(char *s, char c)
+{
+return (strchr(s, c));
+}
