348dd178855b2ddca794c5faebb4f0d5d806619b
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-31 04:45:05 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - a function that searches a string for a set of byes
+ * @s: the string to search
+ * @accept: character to find
+ * Return pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+return (strpbrk(s, accept));
+}
