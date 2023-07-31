029ecb33f90a429298704ce0bb8eee560352cffa
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-07-31 04:50:10 -0700

    fix task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk - a function that searches a string for any of a set of bytes
  * @s: the string to search
  * @accept: character to find
- * Return pointer to the byte in s
+ * Return: pointer to the byte in s
  */
 char *_strpbrk(char *s, char *accept)
 {
