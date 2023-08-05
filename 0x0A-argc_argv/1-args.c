d9b16318674c777fbe3417f929cb169721de5b99
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-08-05 05:46:05 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number of argument
+ * @argv: array of argument
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+(void)argv; 
+return (0);
+}
