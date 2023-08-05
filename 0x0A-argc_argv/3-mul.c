6aa3fa0332c8e02089591b7b6612da2037e8f649
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-08-05 06:55:42 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,15 @@
 int main(int argc, char *argv[])
 {
 int i, mul = 1;
-if (argc == 1)
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-else if (argc >= 1)
-{
 for (i = 1; i < argc; i++)
 {
 mul *= atoi(argv[i]);
 }
 printf("%d\n", mul);
-}
 return (0);
 }
