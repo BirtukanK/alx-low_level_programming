98365913ddd4d5984dac857ac0683c08283a563f
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-08-05 06:53:54 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+int i, mul = 1;
+if (argc == 1)
+{
+printf("Error\n");
+return (1);
+}
+else if (argc >= 1)
+{
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+}
+return (0);
+}
