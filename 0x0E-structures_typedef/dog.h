5ddf9ad8689df9796d54062d62cefc1ec0812bb3
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-08-14 06:10:48 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - a structure to describe dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
