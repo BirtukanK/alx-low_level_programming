d10249773bb4a258a8c0aa799440bffa9fa62658
BirtukanK <birtukankuma1113@gmail.com>
2024-02-06 21:50:27 -0800

    linear search fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	for (i = 0; array && i < size; i++)
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
